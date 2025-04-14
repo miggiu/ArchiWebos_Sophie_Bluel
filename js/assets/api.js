7cbef38d972e65955217a791dfeed81856f46c07
miguo <171590288+miggiu@users.noreply.github.com>
2025-04-14 23:06:21 +0200

    using BASE_API_URL in APi calls (uniformization)

--- js/assets/api.js
+++ js/assets/api.js
@@ -1,10 +1,12 @@
+import { BASE_API_URL } from "./variables.js";
+
 /**
  * Fetches all available categories from the API.
  * Used to populate category filters and dropdown menus.
  * @returns {Promise<Array>} A promise that resolves to an array of category objects
  */
 export async function getCategoriesAndReturn() {
-	const response = await fetch("http://localhost:5678/api/categories");
+	const response = await fetch(`${BASE_API_URL}categories`);
 	const data = await response.json();
 	return data;
 }
@@ -15,7 +17,7 @@ export async function getCategoriesAndReturn() {
  * @returns {Promise<Array>} A promise that resolves to an array of work objects
  */
 export async function getWorksAndReturn() {
-	const response = await fetch("http://localhost:5678/api/works");
+	const response = await fetch(`${BASE_API_URL}works`);
 	const data = await response.json();
 	return data;
 }
